555fefa899c48b18fc0e8a24824dfe0f06d04be5
JQueimado <32784376+JQueimado@users.noreply.github.com>
2019-02-16 13:43:47 +0000

    move client

---
+++ client/m3hthatsends.py
@@ -0,0 +1,63 @@
+import socket
+import os
+
+host = "localhost"
+port = 5000
+
+if __name__ == "__main__":
+
+    s = socket.socket()
+    s.connect((host, port))
+
+    name = input("file name: ")
+
+    f = open(name, 'rb')
+
+    size = os.stat(name).st_size
+
+    s.send( ("sendfile " + name + ' ' + str(size) ).encode() )
+
+    resp = s.recv(1024).decode()
+
+    if (resp != "ack"):
+        
+        print("Error")
+        
+    else:
+
+        totalsize = size
+
+        os.system('setterm -cursor off')
+
+        print ( "file: " + name)
+
+        while(size > 0 ):
+
+            data = f.read(1024)
+
+            s.send(data)
+
+            resp = s.recv(1024).decode()
+
+            if ( resp != "ack" ):
+                print("Error")
+                break
+
+            size -= len(data)
+
+            perc = ( 1 - ( size / totalsize ) ) * 100
+
+            print(
+                "\r%d" % ( perc ) + '%',
+                "[" + int(perc/2) * '|' + int(( 100 - perc )/2) *' ' + "]",
+                end = '\r'
+                )
+
+        print( "Done" )
+
+        os.system('setterm -cursor on')
+
+    f.close()
+    
+
+    pass
